9e5c57e2ebc411831ca66a9dc594c7bf5d57b9e0
ZabdielBH21 <zabdiel.21a@gmail.com>
2023-11-04 16:33:44 -0700

    ss

---
+++ vistas/pagInicio.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="styles/paginaInicio.css">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="js/pagInicio.js">
+<link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Montserrat:wght@700&family=Signika+Negative&display=swap" rel="stylesheet">
+    <title>HappyPet</title>
+</head>
+<body>
+    <!--Este div sera para el menu barra superior-->
+    <div class="conteinerBarra">
+        <nav>
+            <p class="logo" img><img src="Imagenes/happypet2.png" alt=""></p>
+            <ul>
+                <li ><a class="sesion" href="login2.html">Inicio Sesion</a></li>
+                <li>Registrarse</li>
+                <li>Guia</li>
+              
+            </ul>
+        </nav>
+           <!--Este header sera para el titulo de costado-->
+        <header>
+            <h1 >HappyPet</h1>
+            <h2>Dispensador de calidad</h2>
+            <button ><a href="login2.html">Ingresar</a> <img src="iconos/arrowDerecha.svg" alt=""></button>
+        </header>
+
+    </div>
+</body>
+</html>
